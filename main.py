9d39be82d615457915a012164fa66df3bb8419e3
Luiz Otavio Amboni <58530274+luiz-amboni@users.noreply.github.com>
2021-06-11 01:30:45 -0300

    Adicionado validação e formatação de número de telefone

--- main.py
+++ main.py
@@ -1,7 +1,21 @@
-from cpf_cnpj import Documento
+# ------------------------------------------------------ #
 
-exemplo_cnpj = "21647634000100"
-exemplo_cpf = "07387151903"
-documento = Documento.cria_documento(exemplo_cpf)
+# VALIDAÇÃO CPF/CNPJ #
 
-print(documento)
+# from cpf_cnpj import Documento
+#
+# exemplo_cnpj = "21647634000100"
+# exemplo_cpf = "07387151903"
+# documento = Documento.cria_documento(exemplo_cpf)
+#
+# print(documento)
+
+# ------------------------------------------------------ #
+
+from TelefoneBr import TelefoneBr
+
+telefone = "554899175148"
+
+telefone_objeto = TelefoneBr(telefone)
+print(telefone_objeto)
+
